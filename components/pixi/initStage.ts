fe441e043db3f1dbfa69b4d0b88190ef4d79ee04
Richard Unterberg <mail@richardunterberg.de>
2024-04-20 00:21:53 +0200

    🐛 ps - give me this one second back

--- components/pixi/initStage.ts
+++ components/pixi/initStage.ts
@@ -19,7 +19,7 @@ export const initStage = async (stage: HTMLDivElement | null) => {
   const { configCursorRadius } = PixiConfig
 
   // render timeout for mobile devices - pagespeed? :O
-  const timeout = stage.clientWidth < 800 ? 4 : 3
+  const timeout = stage.clientWidth < 800 ? 4 : 2
 
   gsap.delayedCall(timeout, async () => {
     const app = await createApp(stage)
