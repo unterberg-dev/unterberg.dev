e9f0619ad050e4c14737c3222b12c12940d4b709
Richard Unterberg <mail@richardunterberg.de>
2024-05-19 21:09:54 +0200

    ⚗️ test token endpoint

--- components/atoms/BlurDot.tsx
+++ components/atoms/BlurDot.tsx
@@ -1,5 +1,4 @@
-import gsap from 'gsap'
-import { useEffect, useRef } from 'react'
+import { useRef } from 'react'
 import tw from 'tailwind-styled-components'
 
 import { APP_CONFIG } from '#root/lib/constants'
@@ -12,55 +11,8 @@ const StyledBlurDot = tw.div`
   pointer-events-none
 `
 
-interface BlurDotProps extends React.HTMLAttributes<HTMLDivElement> {
-  pulse?: boolean
-  pulseOutOpacity?: number
-  pulseInOpacity?: number
-}
-
-const BlurDot = ({
-  pulse,
-  pulseOutOpacity = 0.2,
-  pulseInOpacity = 0.5,
-  ...props
-}: BlurDotProps) => {
+const BlurDot = ({ ...props }: React.HTMLAttributes<HTMLDivElement>) => {
   const dotRef = useRef<HTMLDivElement>(null)
-  const dotTimelineRef = useRef<gsap.core.Timeline | null>(null)
-
-  // start pulse as soon as pulse prop changes
-  useEffect(() => {
-    const dot = dotRef.current
-    const dotTimeline = dotTimelineRef.current
-    if (!dot || !dotTimeline) return
-
-    if (pulse) {
-      dotTimeline.play()
-    } else {
-      dotTimeline.pause()
-    }
-  }, [pulse])
-
-  // register dot timeline
-  useEffect(() => {
-    const dot = dotRef.current
-
-    // not dot or already registered tl
-    if (!dot || dotTimelineRef.current) return
-
-    const tl = gsap.timeline({ repeat: -1, paused: true, yoyo: true })
-    tl.to(dot, {
-      duration: APP_CONFIG.defaultDurationSlow,
-      opacity: pulseInOpacity,
-      ease: 'power1.in',
-    })
-    tl.to(dot, {
-      duration: APP_CONFIG.defaultDurationSlow,
-      opacity: pulseOutOpacity,
-      ease: 'power1.out',
-    })
-
-    dotTimelineRef.current = tl
-  }, [pulseInOpacity, pulseOutOpacity])
 
   return (
     <StyledBlurDot
@@ -68,9 +20,8 @@ const BlurDot = ({
       ref={dotRef}
       style={{
         backgroundImage: `url(${APP_CONFIG.viteMediaUrl}/decorators/bg/4.svg)`,
-        // backgroundRepeat: 'no-repeat',
+        backgroundRepeat: 'no-repeat',
         // backgroundSize: '100% 100%',
-        ...(pulse === true ? { opacity: pulseOutOpacity } : {}), // set initial opacity to pulseInOpacity if pulse is true
         ...props.style,
       }}
     />
--- components/organisms/ContactModule/index.tsx
+++ components/organisms/ContactModule/index.tsx
@@ -1,10 +1,50 @@
+import { useQuery } from '@tanstack/react-query'
+import { useEffect } from 'react'
+
 import Layout from '#atoms/Layout'
 import HeadlineArea from '#molecules/HeadlineArea'
 
-const ContactModule = () => (
-  <Layout>
-    <HeadlineArea headline="Still wanna collaborate? 😅" subHeadline="Get in touch with me" />
-  </Layout>
-)
+const ContactModule = () => {
+  const {
+    data: token,
+    error,
+    refetch,
+  } = useQuery({
+    queryKey: ['contactFormToken'],
+    enabled: false,
+    queryFn: async () => {
+      const response = await fetch('https://mail.unterberg.dev/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      return response.json()
+    },
+  })
+
+  useEffect(() => {
+    refetch()
+  }, [refetch])
+
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log(token)
+  }, [token])
+
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log(error)
+  }, [error])
+
+  return (
+    <Layout>
+      <HeadlineArea headline="Still wanna collaborate? 😅" subHeadline="Get in touch with me" />
+    </Layout>
+  )
+}
 
 export default ContactModule
